b19c569ee601d549ed3ddf9b63224cbc5efe78c6
Thomas Francois <twfth9@mst.edu>
2019-12-05 02:04:29 -0600

    Improved Serial for IgniterUI
    More to follow, plan is to incorporate serial "handshake" between UI and Arduino, make all serial communication functions into own library/file, and eventually incorporate UI elements to select COM ports

--- Documents/Igniter code/Igniter/Igniter.ino
+++ Documents/Igniter code/Igniter/Igniter.ino
@@ -1,8 +1,8 @@
 /*
-OFFICIAL ROCKET IGNITER CODE
-This .ino file is the main program to be run on the 2560 board
+  OFFICIAL ROCKET IGNITER CODE
+  This .ino file is the main program to be run on the 2560 board
 
- * SD card attached to SPI bus as follows on MEGA2560:
+   SD card attached to SPI bus as follows on MEGA2560:
  ** MOSI = DI - pin 51
  ** MISO = DO - pin 50
  ** CLK - pin 52
@@ -10,37 +10,19 @@ This .ino file is the main program to be run on the 2560 board
 
 */
 #include <SPI.h>
-#include <SD.h>
+#include <SD.h> //Allows SD card reader functions
 
 File myFile;
 
-//Define pressure transducer analog pin variables
+//Define variables for raw data read on analog pins
 int PT1 = A0;
 int PT2 = A1;
 int PT3 = A2;
 int PT4 = A3;
 
-//Define variables for raw data read on analog pins
-int pdata1;
-int pdata2;
-int pdata3;
-int pdata4;
-
-//Define pressure pin voltage variables
-float V1 = A0;
-float V2 = A1;
-float V3 = A2;
-float V4 = A3;
-
-//Define pressure variables
-float P1;
-float P2;
-float P3;
-float P4;
-
 //Time in msec for delay function
-int sleep=200;
-const int TIMEOUT = 5000; 
+int sleep = 200;
+const int TIMEOUT = 5000;
 
 //Define SPI pins for SD card interface
 int CS = 53;
@@ -49,96 +31,123 @@ int MISo = 50;
 int CLK = 52;
 
 //String to receive python UI input
-String receive;
-char ARM[] = "V1V2";
-char FIRE[] = "FIRE";
+char ARM[32] = "ARM";
+char FIRE[32] = "FIRE";
+char inputString[32];
+//bool transferStatus = false;
+int i = 0;
+
+void serial_flush(void) {
+ while (true)
+   {
+   delay (20);  // give data a chance to arrive
+   if (Serial.available ())
+     {
+     // we received something, get all of it and discard it
+     while (Serial.available ())
+       Serial.read ();
+     continue;  // stay in the main loop
+     }
+  else
+    break;  // nothing arrived for 20 ms
+   }
+}
 
 void setup()
 {
-  pinMode(3, OUTPUT);
-  // Open serial communications and wait for port to open:
-  Serial.begin(19200);
-  
-  while (!Serial)
-  {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  //Serial.print("Initializing SD card...");
-
-  if (!SD.begin(CS)) {
-    //Serial.println("initialization failed!");
-    int a = 0;
-    while(a < TIMEOUT)
-    {
-    	a++;
-    };
-  }
-  //Serial.println("initialization done.");
-
-  if (SD.exists("example.txt")) {
-    //Serial.println("example.txt exists.");
-  } else {
-    //Serial.println("example.txt doesn't exist.");
-  }
-
-  // open a new file and immediately close it:
-  //Serial.println("Creating example.txt...");
-  myFile = SD.open("example.txt", FILE_WRITE);
-
-  if (myFile)
-  {
-    //Serial.print("Writing to SD...");
-    myFile.println("Testing");
-    myFile.close();
-  }
-
+  //*****SERIAL INIT*****
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  while (!Serial) {} // wait for serial port to connect. Needed for native USB port only
+
+  Serial.begin(9600);  // Open serial communications and wait for port to open:
+  //*****END OF SERIAL INIT*****
+
+  //*****SD CARD INIT*****
+  /*
+    //pinMode(3, OUTPUT);
+    //Serial.print("Initializing SD card...");
+    if (!SD.begin(CS))
+    {
+      //Serial.println("initialization failed!");
+      int a = 0;
+      while(a < TIMEOUT)
+      {a++;};
+    }
+    //Serial.println("initialization done.");
+    if (SD.exists("example.txt")) {
+      Serial.println("example.txt exists.");
+    } else {
+      Serial.println("example.txt doesn't exist.");
+    }
+    //open a new file and immediately close it:
+    //Serial.println("Creating example.txt...");
+    //myFile = SD.open("example.txt", FILE_WRITE);
+    if (myFile)
+    {
+      //Serial.print("Writing to SD...");
+      myFile.println("Testing");
+      myFile.close();
+    }
+  */
+  //*****END OF SD CARD INIT*****
+
 }
 
 void loop()
-{
-  // Calculate pressure readings from raw values
-  pdata1 = analogRead(PT1);
-  V1 = pdata1*(5.0/1023.0);
-  P1 = (14000/2500)*V1;
-
-  pdata2 = analogRead(PT2);
-  V2 = pdata2*(5.0/1023.0);
-  P2 = (14000/2500)*V2;
-
-  pdata3 = analogRead(PT3);
-  V3 = pdata3*(5.0/1023.0);
-  P3 = (14000/2500)*V3;
-
-  pdata4 = analogRead(PT4);
-  V4 = pdata4*(5.0/1023.0);
-  P4 = (14000/2500)*V4;
-
-  // Print pressure readings to serial bus to be read by the UI
-  Serial.print(String(P1)+'a'+String(P2)+'b'+String(P3)+'c'+String(P4)+'d');
-
-}
-
-void serialEvent()
-{
-  String buff;
-  while(Serial.available())
-  {
-  char inChar = Serial.read();
-  buff += inChar;
-  // Listen for commands sent from UI over serial
+{  
   
-  if (buff.endsWith('e'))
+  //Transmit as chars
+    Serial.print(analogRead(PT1));
+    Serial.print('a');
+    Serial.print('\3');    
+    Serial.print(analogRead(PT2));
+    Serial.print('b');
+    Serial.print('\3');
+    Serial.print(analogRead(PT3));
+    Serial.print('c');
+    Serial.print('\3');
+    Serial.print(analogRead(PT4));
+    Serial.print('d');
+    Serial.print('\3');
+
+/*
+   //Transmit as hex
+    Serial.write(analogRead(PT1));
+    Serial.write(0x1D);
+    Serial.write(analogRead(PT2));
+    Serial.write(0x1D);
+    Serial.write(analogRead(PT3));
+    Serial.write(0x1D);
+    Serial.write(analogRead(PT4));
+    Serial.write(0x1D);
+*/
+
+  if (Serial.available())
   {
-    digitalWrite(LED_BUILTIN, HIGH);
-    buff = "";
-  }
-  if (buff == FIRE)
-  {
-    digitalWrite(LED_BUILTIN, LOW);
-    buff = "";
-  }
+    char inByte = Serial.read();
+    if (inByte == '\3')
+    {
+      transferStatus = true;
+      inputString[i] = '\0';
+      if (strcmp(inputString, ARM)==0)
+      {
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);                       // wait for a second
+        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+        memset(inputString, 0, 32);
+      }
+      if (strcmp(inputString, FIRE)==0)
+      {
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);                       // wait for a second
+        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+        memset(inputString, 0, 32);
+      }
+      i = -1;
+      serial_flush();
+    }
+    else
+      inputString[i] = inByte;
+    i++;
   }
 }
--- Documents/Igniter code/IgniterUI/IgniterUI.py
+++ Documents/Igniter code/IgniterUI/IgniterUI.py
@@ -54,26 +54,28 @@ window = sg.Window('RocketView', layout, grab_anywhere=True)
 
 # Open serial port and print which port is connected
 # Also declare buffer string to store serial data
-ser = serial.Serial('COM4',19200,timeout=1)
+ser = serial.Serial('COM3',9600,timeout=1)
 print(ser.name)
 buffer = ""
 
 # Command to open valves for ARM event
-arm = "eee"
+arm = "ARM"
 fire = "FIRE"
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event, values = window.read(timeout=100)
+    event, values = window.read(timeout=10)
     # Button Reactions
     if event is None:
         break
     if event == 'ARM':
-        #print('ARMED')
+        print('ARMED')
         isValveOpen[1] = True
         isValveOpen[2] = True
         # Send command to arm
+        print("Sending" + arm)
         ser.write(bytearray(arm, 'utf-8'))
+        ser.write(bytearray('\3', 'utf-8'))
     elif event == 'FIRE':
         #print('FIRING')
         # timer = threading.Timer(1, igniterLoop)
@@ -87,6 +89,7 @@ while True:
         isValveOpen[2] = False
         isValveOpen[0] = False
         ser.write(bytearray(fire, 'utf-8'))
+        ser.write(bytearray('\3', 'utf-8'))
     elif event == 'PURGE':
         #print('Valve 5 open')
         isValveOpen[5] = True
@@ -105,30 +108,33 @@ while True:
         else:
             window.FindElement(x).Update(button_color=('White', 'Red'))
 
-    # Update pressure readings
-    buffer = buffer + ser.read().decode('utf-8')
-    print(buffer)
-    if (buffer.endswith('a') == 1):
-        length = len(buffer)
-        buffer = buffer[0:(length-1)]
-        window.Element('P1').Update(buffer)
-        buffer = ""
-    if (buffer.endswith('b') == 1):
-        length = len(buffer)
-        buffer = buffer[0:(length - 1)]
-        window.Element('P2').Update(buffer)
-        buffer = ""
-    if (buffer.endswith('c') == 1):
-        length = len(buffer)
-        buffer = buffer[0:(length - 1)]
-        window.Element('P3').Update(buffer)
-        buffer = ""
-    if (buffer.endswith('d') == 1):
-        length = len(buffer)
-        buffer = buffer[0:(length - 1)]
-        window.Element('P4').Update(buffer)
-        buffer = ""
+    #*****UPDATE PRESSURE READINGS*****
+    buffer = buffer + ser.read().decode('utf-8') #assmble string buffer of inbound serial data
+    # s = ser.read()
+    #hex_string = binascii.hexlify(s).decode('utf-8')
 
-    #print("NO EVENT")
+    # Parses serial buffer from microcontroller
+    # Expects serial data w/ ',' as delimiter between data elements
+    # Ex: 123a,456b,789c,...
+    # Converts raw data into float value, used to update GUI
+    print(buffer)
+    if '\3' in buffer:
+        buffer = buffer.replace('\3','')                 # Remove delimiter from buffer string
+        print(buffer)
+        identifier = buffer[len(buffer)-1]              # Store identifier
+        buffer = buffer[:-1]                            # Remove identifier from buffer string
+        pressureVal = float(buffer)*(7.0/256.0)         # See calculation note below
+            #Pressure Calculation = pdata#*(5.0/1023.0)(14000/2500)
+            #Simplified to constant: (7.0/256.0)
+        pressureVal = round(pressureVal, 2)             # Round value to 2 decimal places
+        if identifier == 'a':
+            window.Element('P1').Update(pressureVal)
+        elif identifier == 'b':
+             window.Element('P2').Update(pressureVal)
+        elif identifier == 'c':
+             window.Element('P3').Update(pressureVal)
+        elif identifier == 'd':
+             window.Element('P4').Update(pressureVal)
+        buffer = ""                                     # Clear buffer
 window.close()
 

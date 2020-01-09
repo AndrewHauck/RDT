067f3c722fe74828044c77e30fd358531d169e28
Thomas Francois <twfth9@mst.edu>
2020-01-08 21:32:07 -0600

    Added COM Port Dropdown to IgniterUI and Basic Enquire Button
    Enquire button turns green after acknowledgement from arduino, but does not return to red by itself.

--- Documents/Igniter code/Ground Station/IgniterUI.py
+++ Documents/Igniter code/Ground Station/IgniterUI.py
@@ -1,8 +1,9 @@
 import PySimpleGUI as sg
 import threading
 import serial
+import serial.tools.list_ports
 import time
-
+from GSModules import ListComPorts
 # Sets background and button color
 sg.change_look_and_feel('DarkAmber')
 
@@ -19,6 +20,13 @@ sg.change_look_and_feel('DarkAmber')
 #     isValveOpen[0] = False
 
 # List of items inside of window
+# COM Port Selector
+COMSelector = [[sg.Text('COM Port')],
+               [sg.Combo(values=ListComPorts.serial_ports(), enable_events=False, size=(20, 3), key='COM_Combo'),
+                sg.Button('Refresh', key='COM_Refresh'),
+                sg.Button('Open', button_color=('White', 'Red'), key='COM_Connect'),
+                sg.Button('Enquire', button_color=('White', 'Red'), key='Enquire')]]
+
 # Buttons for valves
 valves = [[sg.Text('VALVES')],
           [sg.Button('Main CH4 (1)', button_color=('White', 'Red'), key=1),
@@ -26,10 +34,11 @@ valves = [[sg.Text('VALVES')],
           sg.Button('Fire CH4 (3)', button_color=('White', 'Red'), key=3),
           sg.Button('Fire GOX (4)', button_color=('White', 'Red'), key=4),
           sg.Button('Purge (5)', button_color=('White', 'Red'), key=5),
-          sg.Button('Igniter (0)', button_color=('White', 'Red'), key=0)]]
+          sg.Button('Igniter (0)', button_color=('White', 'Red'), key=0),]
+]
 
 # Igniter diagram
-diagram = [[sg.Image('IgniterDiagram.png', key='DIAGRAM', size=(400, 400))]]
+diagram = [[sg.Image('IgniterDiagram.png', key='DIAGRAM', size=(400, 400))],]
 
 # Staged buttons
 stages = [[sg.Text('STAGES')],
@@ -39,35 +48,24 @@ stages = [[sg.Text('STAGES')],
           sg.Button('CLOSE ALL', button_color=('White', 'Red'), key='CLOSE ALL')]]
 
 # Sensor reading text elements
-readings = [[sg.Text('Pressure 1:'), sg.Text('0000000000', key='P1'), sg.Text('PSI')],
-            [sg.Text('Pressure 2:'), sg.Text('0000000000', key='P2'), sg.Text('PSI')],
-            [sg.Text('Pressure 3:'), sg.Text('0000000000', key='P3'), sg.Text('PSI')],
-            [sg.Text('Pressure 4:'), sg.Text('0000000000', key='P4'), sg.Text('PSI')]]
+readings = [[sg.Text('Pressure 1:'), sg.Text('XXXXXXXXXX', key='P1'), sg.Text('PSI')],
+            [sg.Text('Pressure 2:'), sg.Text('XXXXXXXXXX', key='P2'), sg.Text('PSI')],
+            [sg.Text('Pressure 3:'), sg.Text('XXXXXXXXXX', key='P3'), sg.Text('PSI')],
+            [sg.Text('Pressure 4:'), sg.Text('XXXXXXXXXX', key='P4'), sg.Text('PSI')]]
 
 # Combination of all elements
-layout = valves + diagram + stages + readings
+#layout = valves + COMSelector + diagram + stages + readings
+layout = valves + COMSelector + diagram + stages + readings
 
 # Valve states
 isValveOpen = [False, False, False, False, False, False]
 # Create the Window and Finalize it. Then fullscreen
-window = sg.Window('RocketView', layout, grab_anywhere=True)
+window = sg.Window('RocketView', layout, grab_anywhere=False)
 
-# Open serial port and print which port is connected
-# Also declare buffer string to store serial data
-
-ser = serial.Serial('COM3',9600,timeout=1)
-print(ser.name)
+# Declaring buffer string to store serial data
 buffer = ""
-
-# Command to open valves for ARM event
-arm = "ARM"
-fire = "FIRE"
 dataString = ""
-
-###FIGURE OUT HOW TO ADD A DROP DOWN MENU FOR SELECTING COM PORTS
-
-##FIND WAY TO ADD "ENQUIRE" BUTTON
-
+ser = serial.Serial(None) #Create serial port object
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -75,6 +73,33 @@ while True:
     # Button Reactions
     if event is None:
         break
+    if event == 'COM_Connect':
+        if not ser.is_open:
+            try:    #OPENING SERIAL PORT
+                ser = serial.Serial(values['COM_Combo'], 9600, timeout=1)
+                if __name__ == '__main__':
+                    window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
+                    window.FindElement('COM_Connect').update(values['COM_Combo'], button_color=('White', 'Green'))
+                    window.FindElement('Enquire').update(button_color=('White', 'Red'))
+            except serial.SerialException: #IF PORT CAN'T BE OPENED
+                print("Unable to Open Port")
+        else:   #CLOSING SERIAL PORT
+            ser.close()
+            window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
+            window.FindElement('COM_Connect').update('Open', button_color=('White', 'Red'))
+            window.FindElement('Enquire').update(button_color=('White', 'Red'))
+            window.Element('P1').Update("XXXXXXXXXX")
+            window.Element('P2').Update("XXXXXXXXXX")
+            window.Element('P3').Update("XXXXXXXXXX")
+            window.Element('P4').Update("XXXXXXXXXX")
+    if event == 'COM_Refresh':
+        if __name__ == '__main__':
+            window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
+    if event == 'Enquire':
+        try:
+            ser.write(bytearray([0x05]))
+        except serial.SerialException:
+            print("Port Closed")
     if event == 'ARM':
         print("arming...")
         isValveOpen[1] = True
@@ -118,55 +143,59 @@ while True:
     #*****UPDATE PRESSURE READINGS*****
     # Parses serial buffer from microcontroller
     # Converts raw data into float value, used to update GUI
-    while ser.in_waiting:
-        inChar = ser.read().decode('ASCII')
-        buffer = buffer + inChar
-        if(inChar == chr(4)):
-            if(buffer.find(chr(1)) >= 0):
-                buffer = buffer[buffer.find(chr(1)):(buffer.find(chr(4))+1)] # Trim off excess characters before and after packet
-                packet_type = buffer[1]
+    if ser.is_open:
+        while ser.in_waiting:
+            inChar = ser.read().decode('ASCII')
+            buffer = buffer + inChar
+            if(inChar == chr(6)):
+                buffer = ""
+                window.FindElement('Enquire').update(button_color=('White', 'Green'))
+            if(inChar == chr(4)):
+                if(buffer.find(chr(1)) >= 0):
+                    buffer = buffer[buffer.find(chr(1)):(buffer.find(chr(4))+1)] # Trim off excess characters before and after packet
+                    packet_type = buffer[1]
 
-                upperNibble: int = ord(buffer[2])
-                lowerNibble: int = ord(buffer[3])
-                # UPPER NIBBLE
-                if (upperNibble > 0x40): # if a letter
-                    upperNibble = upperNibble & 0b00001111 # strip off upper nibble(only indicates that value is a letter)
-                    upperNibble = upperNibble + 0b00001001 # upperNibble now equals 0x0A - 0x0F
+                    upperNibble: int = ord(buffer[2])
+                    lowerNibble: int = ord(buffer[3])
+                    # UPPER NIBBLE
+                    if (upperNibble > 0x40): # if a letter
+                        upperNibble = upperNibble & 0b00001111 # strip off upper nibble(only indicates that value is a letter)
+                        upperNibble = upperNibble + 0b00001001 # upperNibble now equals 0x0A - 0x0F
+                    else:
+                        upperNibble = upperNibble & 0b00001111 # upperNibble now equals 0x00 - 0x09
+                    upperNibble = upperNibble << 4 # upperNibble now represents upper byte of dataBytes
+                    # LOWER NIBBLE
+                    if (lowerNibble > 0x40): # if letter
+                        lowerNibble = lowerNibble & 0b00001111 # strip off upper nibble
+                        lowerNibble = lowerNibble + 0b00001001 # lowerNibble now equals 0x0A - 0x0F
+                    lowerNibble = lowerNibble & 0b00001111 # lowerNibble now equals 0x00 - 0x09 if it was a number
+
+                    packet_dataSize = upperNibble | lowerNibble # combining upper and lower bytes to make final char(UUUU LLLL)
+                    dataString = buffer[5:-1] # sets data string of packet to data pulled from _inputString
+                    buffer = "" # Clear buffer
+
+                    # ***  UPDATE PRESSURE READINGS ***
+                    # ***  CAN WE CHECK THESE CALCULATIONS???
+                    # ***  5V ON PIN = 28 PSI???
+                    if(packet_type == 'D'):
+                        pdata1 = dataString[dataString.find('A')+1:dataString.find('B')]
+                        pdata1 = int(pdata1)*(5.0/1023.0)*(14000.0/2500.0)
+                        window.Element('P1').Update(round(pdata1, 2))
+
+                        pdata2 = dataString[dataString.find('B')+1:dataString.find('C')]
+                        pdata2 = int(pdata2)*(5.0/1023.0)*(14000.0/2500.0)
+                        window.Element('P2').Update(round(pdata2, 2))
+
+                        pdata3 = dataString[dataString.find('C')+1:dataString.find('D')]
+                        pdata3 = int(pdata3)*(5.0/1023.0)*(14000.0/2500.0)
+                        window.Element('P3').Update(round(pdata3, 2))
+
+                        pdata4 = dataString[dataString.find('D')+1:]
+                        pdata4 = int(pdata4)*(5.0/1023.0)*(14000.0/2500.0)
+                        window.Element('P4').Update(round(pdata4, 2))
+                    if(packet_type == 'M'):
+                        print(dataString)
                 else:
-                    upperNibble = upperNibble & 0b00001111 # upperNibble now equals 0x00 - 0x09
-                upperNibble = upperNibble << 4 # upperNibble now represents upper byte of dataBytes
-                # LOWER NIBBLE
-                if (lowerNibble > 0x40): # if letter
-                    lowerNibble = lowerNibble & 0b00001111 # strip off upper nibble
-                    lowerNibble = lowerNibble + 0b00001001 # lowerNibble now equals 0x0A - 0x0F
-                lowerNibble = lowerNibble & 0b00001111 # lowerNibble now equals 0x00 - 0x09 if it was a number
-
-                packet_dataSize = upperNibble | lowerNibble # combining upper and lower bytes to make final char(UUUU LLLL)
-                dataString = buffer[5:-1] # sets data string of packet to data pulled from _inputString
-                buffer = "" # Clear buffer
-
-                # ***  UPDATE PRESSURE READINGS ***
-                # ***  CAN WE CHECK THESE CALCULATIONS???
-                # ***  5V ON PIN = 28 PSI???
-                if(packet_type == 'D'):
-                    pdata1 = dataString[dataString.find('A')+1:dataString.find('B')]
-                    pdata1 = int(pdata1)*(5.0/1023.0)*(14000.0/2500.0)
-                    window.Element('P1').Update(round(pdata1, 2))
-
-                    pdata2 = dataString[dataString.find('B')+1:dataString.find('C')]
-                    pdata2 = int(pdata2)*(5.0/1023.0)*(14000.0/2500.0)
-                    window.Element('P2').Update(round(pdata2, 2))
-
-                    pdata3 = dataString[dataString.find('C')+1:dataString.find('D')]
-                    pdata3 = int(pdata3)*(5.0/1023.0)*(14000.0/2500.0)
-                    window.Element('P3').Update(round(pdata3, 2))
-
-                    pdata4 = dataString[dataString.find('D')+1:]
-                    pdata4 = int(pdata4)*(5.0/1023.0)*(14000.0/2500.0)
-                    window.Element('P4').Update(round(pdata4, 2))
-                if(packet_type == 'M'):
-                    print(dataString)
-            else:
-                buffer = "" # If only 1 end of packet is found, data somehow corrupted, clear buffer
+                    buffer = "" # If only 1 end of packet is found, data somehow corrupted, clear buffer
 window.close()
 

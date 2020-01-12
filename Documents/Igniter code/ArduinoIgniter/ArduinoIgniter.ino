5b85d56305f6816c7e1f2667549ee5af0fc7abd3
Thomas Francois <twfth9@mst.edu>
2020-01-11 22:15:02 -0600

    Added/Implemented Valves Object
    Valves Objects are used to ease in the control and monitoring of 6 valves on various pins of the arduino. This push is because I'm looking for another IDE cause the arduino one kinda sucks lol.

--- Documents/Igniter code/ArduinoIgniter/ArduinoIgniter.ino
+++ Documents/Igniter code/ArduinoIgniter/ArduinoIgniter.ino
@@ -1,54 +1,35 @@
 #include "src/serialPacket/serialPacket.h"
 #include "src/serialBuffer/serialBuffer.h"
+#include "src/valves/valves.h"
 
 #define OPEN HIGH
 #define CLOSE LOW
 
-serialBuffer buffer1;
-serialPacket outPacket;
-//Pressure transducer analog pin variables
-const int PT[4] = {A0, A1, A2, A3};
+serialBuffer buffer1; //Serial Buffer object used to store incoming serial data
+serialPacket outPacket; //Packet object used to format data being sent out serial port
 
-//Variables for raw data read on analog pins
-int pdata[4] = {0};
+const int PT[4] = {A0, A1, A2, A3}; //Pressure transducer analog pin variables
+int pdata[4] = {0}; //Variables for raw data read on analog pins
 
 //Pins for valves/pin_Igniter
 // [0],     [1],     [2],     [3],     [4], [5]
 // CH4Main, GOXMain, CH4Fire, GOXFire, N2,  Igniter
-const int valvePins[6] = {1,2,3,4,5,6};
-/*
-const int pin_CH4Main = 1;
-const int pin_GOXMain = 2;
-const int pin_CH4Fire = 3;
-const int pin_GOXFire = 4;
-const int pin_N2 = 5;
-const int pin_Igniter = 6;
-*/
+valves vs;  //Valves object used to store and control pin states for valves
 
-//Current states of each valve/igniter
-// [0],     [1],     [2],     [3],     [4], [5]
-// CH4Main, GOXMain, CH4Fire, GOXFire, N2,  Igniter
-bool valveStates[6];
 
-//Variable for stage
-char stage = 0;
 //float refresh_rate = 0.3
 
 void setup() {
   Serial.begin(9600);   // initialize serial
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  for(int i=0; i<6; i++){
-    pinMode(valvePins[i], OUTPUT);
-    digitalWrite(valvePins[i], CLOSE);
-    valveStates[i] = CLOSE;
-  }
+  int pins[6] = {1,2,3,4,5,6};
+  vs.setPins(pins);
 }
 
 void loop() {
   for(int i=0; i<4; i++)
     pdata[i] = analogRead(PT[i]);
-  //String data = String('A' + String(pdata[0]) + 'B' + String(pdata[1]) + 'C' + String(pdata[2]) + 'D' + String(pdata[3]));
   outPacket.configure(String('A' + String(pdata[0]) + 'B' + String(pdata[1]) + 'C' + String(pdata[2]) + 'D' + String(pdata[3])), 'D');
   outPacket.transmit();
   
@@ -64,21 +45,9 @@ void loop() {
       if(inPacket.getData() == "ARM")
       {
         digitalWrite(LED_BUILTIN, HIGH);
-        stage = 1;
-        valveStates[0] = OPEN;
-        valveStates[1] = OPEN;
-        valveStates[2] = CLOSE;
-        valveStates[3] = CLOSE;
-        valveStates[4] = CLOSE;
-        valveStates[5] = CLOSE;
-        
-        outPacket.configure("", 'V');
-        for(int i=0; i<6; i++)
-        {
-          digitalWrite(valvePins[i], valveStates[i]);
-          outPacket.append(valveStates[i]+0x30);
-        }
-        outPacket.transmit();
+        bool states[6] = {OPEN, OPEN, CLOSE, CLOSE, CLOSE, CLOSE};
+        vs.setStates(states);
+        vs.transmit();
         
         outPacket.configure("IGNITER ARMED", 'M');
         outPacket.transmit();
@@ -86,21 +55,9 @@ void loop() {
       if(inPacket.getData() == "FIRE")
       {
         digitalWrite(LED_BUILTIN, LOW);
-        stage = 2;
-        valveStates[0] = OPEN;
-        valveStates[1] = OPEN;
-        valveStates[2] = OPEN;
-        valveStates[3] = OPEN;
-        valveStates[4] = CLOSE;
-        valveStates[5] = CLOSE;
-        
-        outPacket.configure("", 'V');
-        for(int i=0; i<6; i++)
-        {
-          digitalWrite(valvePins[i], valveStates[i]+0x30);
-          outPacket.append(valveStates[i]+0x30);
-        }
-        outPacket.transmit();
+        bool states[6] = {OPEN, OPEN, OPEN, OPEN, CLOSE, CLOSE};
+        vs.setStates(states);
+        vs.transmit();
                 
         outPacket.configure("IGNITER FIRING", 'M');
         outPacket.transmit();
@@ -108,21 +65,9 @@ void loop() {
       if(inPacket.getData() == "PURGE")
       {
         digitalWrite(LED_BUILTIN, HIGH);
-        stage = 3;
-        valveStates[0] = CLOSE;
-        valveStates[1] = CLOSE;
-        valveStates[2] = OPEN;
-        valveStates[3] = OPEN;
-        valveStates[4] = OPEN;
-        valveStates[5] = CLOSE;
-        
-        outPacket.configure("", 'V');
-        for(int i=0; i<6; i++)
-        {
-          digitalWrite(valvePins[i], valveStates[i]);
-          outPacket.append(valveStates[i]+0x30);
-        }
-        outPacket.transmit();
+        bool states[6] = {CLOSE, CLOSE, OPEN, OPEN, OPEN, CLOSE};
+        vs.setStates(states);
+        vs.transmit();
         
         outPacket.configure("IGNITER PURGING", 'M');
         outPacket.transmit();
@@ -130,21 +75,9 @@ void loop() {
       if(inPacket.getData() == "CLOSE")
       {
         digitalWrite(LED_BUILTIN, LOW);
-
-        valveStates[0] = CLOSE;
-        valveStates[1] = CLOSE;
-        valveStates[2] = CLOSE;
-        valveStates[3] = CLOSE;
-        valveStates[4] = CLOSE;
-        valveStates[5] = CLOSE;
-        
-        outPacket.configure("", 'V');
-        for(int i=0; i<6; i++)
-        {
-          digitalWrite(valvePins[i], valveStates[i]);
-          outPacket.append(valveStates[i]+0x30);
-        }
-        outPacket.transmit();
+        bool states[6] = {CLOSE, CLOSE, CLOSE, CLOSE, CLOSE, CLOSE};
+        vs.setStates(states);
+        vs.transmit();
         
         outPacket.configure("IGNITER CLOSED", 'M');
         outPacket.transmit();
@@ -153,15 +86,8 @@ void loop() {
     if(inPacket.getType() == 'V')
     {
       char pin = inPacket.getData().toInt();
-      valveStates[pin] = !valveStates[pin];
-      
-      outPacket.configure("", 'V');
-      for(int i=0; i<6; i++)
-      {
-        digitalWrite(valvePins[i], valveStates[i]);
-        outPacket.append(valveStates[i]+0x30);
-      }
-      outPacket.transmit();
+      vs.toggleSingle(pin);
+      vs.transmit();
 
       outPacket.configure("VALVE TOGGLED", 'M');
       outPacket.transmit();
--- Documents/Igniter code/Ground Station/IgniterUI.py
+++ Documents/Igniter code/Ground Station/IgniterUI.py
@@ -42,12 +42,9 @@ while True:
     # Button Reactions
     if event is None:
         break
-    elif event == 'dropdown':
-        #filename = sg.popup_get_file('file to open', no_window=True)
-        print("Got em")
-    elif event == 'COM_Connect':
-        if not ser.is_open:
-            try:    #OPENING SERIAL PORT
+    if not ser.is_open:
+        if event == 'COM_Connect':
+            try:  # OPENING SERIAL PORT
                 ser = serial.Serial(values['COM_Combo'], 9600, timeout=1)
                 ser.write(bytearray([0x05]))
                 if __name__ == '__main__':
@@ -57,7 +54,8 @@ while True:
                     f = open("test.txt", "w+")
             except serial.SerialException: #IF PORT CAN'T BE OPENED
                 print("Unable to Open Port")
-        else:   #CLOSING SERIAL PORT
+    else:
+        if event == 'COM_Connect':  # CLOSING SERIAL PORT
             ser.close()
             f.close()
             window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
@@ -72,143 +70,147 @@ while True:
             window.Element('PURGE').Update(button_color=('White', 'Red'))
             window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
             for x in range(6):
+                window.Element("valve" + str(x)).Update(button_color=('White', 'Red'))
+        elif event == 'dropdown':
+            #filename = sg.popup_get_file('file to open', no_window=True)
+            print("Got em")
+        elif event == 'COM_Refresh':
+            print(datetime.datetime.now())
+            if __name__ == '__main__':
+                window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
+        elif event == 'COM_Enquire':
+            try:
+                ser.write(bytearray([0x05]))
+            except serial.SerialException:
+                print("Port Closed")
+        elif event == "valve" + str(0):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x30, 0x04]
+            ser.write(bytearray(packet))
+        elif event == "valve" + str(1):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x31, 0x04]
+            ser.write(bytearray(packet))
+        elif event == "valve" + str(2):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x32, 0x04]
+            ser.write(bytearray(packet))
+        elif event == "valve" + str(3):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x33, 0x04]
+            ser.write(bytearray(packet))
+        elif event == "valve" + str(4):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x34, 0x04]
+            ser.write(bytearray(packet))
+        elif event == "valve" + str(5):
+            packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x35, 0x04]
+            ser.write(bytearray(packet))
+        elif event == 'ARM':
+            print("arming...")
+            # Send command to arm
+            packet = [0x01, 0x43, 0x30, 0x33, 0x02, 0x41, 0x52, 0x4d, 0x04]
+            ser.write(bytearray(packet))
+        elif event == 'FIRE':
+            print("firing...")
+            packet = [0x01, 0x43, 0x30, 0x34, 0x02, 0x46, 0x49, 0x52, 0x45, 0x04]
+            ser.write(bytearray(packet))
+        elif event == 'PURGE':
+            print("purging...")
+            packet = [0x01, 0x43, 0x30, 0x35, 0x02, 0x50, 0x55, 0x52, 0x47, 0x45, 0x04]
+            ser.write(bytearray(packet))
+        elif event == 'CLOSE ALL':
+            #print('Valve 5 closed')
+            #print('Valve 3 closed. Valve 4 closed.')
+            print("closing...")
+            packet = [0x01, 0x43, 0x30, 0x35, 0x02, 0x43, 0x4c, 0x4f, 0x53, 0x45, 0x04]
+            ser.write(bytearray(packet))
+
+        # Updating GUI to reflect valve states
+        for x in range(6):
+            if isValveOpen[x] == True:
+                window.FindElement("valve" + str(x)).Update(button_color=('White', 'Green'))
+            else:
                 window.FindElement("valve" + str(x)).Update(button_color=('White', 'Red'))
-    elif event == 'COM_Refresh':
-        print(datetime.datetime.now())
-        if __name__ == '__main__':
-            window.FindElement('COM_Combo').update(values=ListComPorts.serial_ports())
-    elif event == 'COM_Enquire':
-        try:
-            ser.write(bytearray([0x05]))
-        except serial.SerialException:
-            print("Port Closed")
-    elif event == "valve" + str(0):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x30, 0x04]
-        ser.write(bytearray(packet))
-    elif event == "valve" + str(1):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x31, 0x04]
-        ser.write(bytearray(packet))
-    elif event == "valve" + str(2):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x32, 0x04]
-        ser.write(bytearray(packet))
-    elif event == "valve" + str(3):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x33, 0x04]
-        ser.write(bytearray(packet))
-    elif event == "valve" + str(4):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x34, 0x04]
-        ser.write(bytearray(packet))
-    elif event == "valve" + str(5):
-        packet = [0x01, 0x56, 0x30, 0x31, 0x02, 0x35, 0x04]
-        ser.write(bytearray(packet))
-    elif event == 'ARM':
-        print("arming...")
-        # Send command to arm
-        packet = [0x01, 0x43, 0x30, 0x33, 0x02, 0x41, 0x52, 0x4d, 0x04]
-        ser.write(bytearray(packet))
-    elif event == 'FIRE':
-        print("firing...")
-        packet = [0x01, 0x43, 0x30, 0x34, 0x02, 0x46, 0x49, 0x52, 0x45, 0x04]
-        ser.write(bytearray(packet))
-    elif event == 'PURGE':
-        print("purging...")
-        packet = [0x01, 0x43, 0x30, 0x35, 0x02, 0x50, 0x55, 0x52, 0x47, 0x45, 0x04]
-        ser.write(bytearray(packet))
-    elif event == 'CLOSE ALL':
-        #print('Valve 5 closed')
-        #print('Valve 3 closed. Valve 4 closed.')
-        print("closing...")
-        packet = [0x01, 0x43, 0x30, 0x35, 0x02, 0x43, 0x4c, 0x4f, 0x53, 0x45, 0x04]
-        ser.write(bytearray(packet))
 
-    # Updating GUI to reflect valve states
-    for x in range(6):
-        if isValveOpen[x] == True:
-            window.FindElement("valve" + str(x)).Update(button_color=('White', 'Green'))
-        else:
-            window.FindElement("valve" + str(x)).Update(button_color=('White', 'Red'))
+        #*****UPDATE PRESSURE READINGS*****
+        # Parses serial buffer from microcontroller
+        # Converts raw data into float value, used to update GUI
+        if ser.is_open:
+            while ser.in_waiting:
+                inChar = ser.read().decode('ASCII')
+                buffer = buffer + inChar
+                if (inChar == chr(6)):
+                    buffer = ""
+                    window.FindElement('COM_Enquire').update(button_color=('White', 'Green'))
+                if (inChar == chr(4)):
+                    if (buffer.find(chr(1)) >= 0):
+                        buffer = buffer[buffer.find(chr(1)):(
+                                    buffer.find(chr(4)) + 1)]  # Trim off excess characters before and after packet
+                        packet_type = buffer[1]
 
-    #*****UPDATE PRESSURE READINGS*****
-    # Parses serial buffer from microcontroller
-    # Converts raw data into float value, used to update GUI
-    if ser.is_open:
-        while ser.in_waiting:
-            inChar = ser.read().decode('ASCII')
-            buffer = buffer + inChar
-            if (inChar == chr(6)):
-                buffer = ""
-                window.FindElement('COM_Enquire').update(button_color=('White', 'Green'))
-            if (inChar == chr(4)):
-                if (buffer.find(chr(1)) >= 0):
-                    buffer = buffer[buffer.find(chr(1)):(
-                                buffer.find(chr(4)) + 1)]  # Trim off excess characters before and after packet
-                    packet_type = buffer[1]
+                        upperNibble: int = ord(buffer[2])
+                        lowerNibble: int = ord(buffer[3])
+                        # UPPER NIBBLE
+                        if (upperNibble > 0x40):  # if a letter
+                            upperNibble = upperNibble & 0b00001111  # strip off upper nibble(only indicates that value is a letter)
+                            upperNibble = upperNibble + 0b00001001  # upperNibble now equals 0x0A - 0x0F
+                        else:
+                            upperNibble = upperNibble & 0b00001111  # upperNibble now equals 0x00 - 0x09
+                        upperNibble = upperNibble << 4  # upperNibble now represents upper byte of dataBytes
+                        # LOWER NIBBLE
+                        if (lowerNibble > 0x40):  # if letter
+                            lowerNibble = lowerNibble & 0b00001111  # strip off upper nibble
+                            lowerNibble = lowerNibble + 0b00001001  # lowerNibble now equals 0x0A - 0x0F
+                        lowerNibble = lowerNibble & 0b00001111  # lowerNibble now equals 0x00 - 0x09 if it was a number
 
-                    upperNibble: int = ord(buffer[2])
-                    lowerNibble: int = ord(buffer[3])
-                    # UPPER NIBBLE
-                    if (upperNibble > 0x40):  # if a letter
-                        upperNibble = upperNibble & 0b00001111  # strip off upper nibble(only indicates that value is a letter)
-                        upperNibble = upperNibble + 0b00001001  # upperNibble now equals 0x0A - 0x0F
+                        packet_dataSize = upperNibble | lowerNibble  # combining upper and lower bytes to make final char(UUUU LLLL)
+                        dataString = buffer[5:-1]  # sets data string of packet to data pulled from _inputString
+                        buffer = ""  # Clear buffer
+
+                        # ***  UPDATE PRESSURE READINGS ***
+                        # ***  CAN WE CHECK THESE CALCULATIONS???
+                        # ***  5V ON PIN = 28 PSI???
+                        if (packet_type == 'D'):
+                            pdata1 = dataString[dataString.find('A') + 1:dataString.find('B')]
+                            pdata1 = int(pdata1) * (5.0 / 1023.0) * (14000.0 / 2500.0)
+                            window.Element('P1').Update(round(pdata1, 2))
+                            f.write(str(datetime.now().time()) + ": Pressure 1: " + str(pdata1) + "\r\n")
+                            pdata2 = dataString[dataString.find('B') + 1:dataString.find('C')]
+                            pdata2 = int(pdata2) * (5.0 / 1023.0) * (14000.0 / 2500.0)
+                            window.Element('P2').Update(round(pdata2, 2))
+                            f.write(str(datetime.now().time()) + ": Pressure 2: " + str(pdata2) + "\r\n")
+                            pdata3 = dataString[dataString.find('C') + 1:dataString.find('D')]
+                            pdata3 = int(pdata3) * (5.0 / 1023.0) * (14000.0 / 2500.0)
+                            window.Element('P3').Update(round(pdata3, 2))
+                            f.write(str(datetime.now().time()) + ": Pressure 3: " + str(pdata3) + "\r\n")
+                            pdata4 = dataString[dataString.find('D') + 1:]
+                            pdata4 = int(pdata4) * (5.0 / 1023.0) * (14000.0 / 2500.0)
+                            window.Element('P4').Update(round(pdata4, 2))
+                            f.write(str(datetime.now().time()) + ": Pressure 4: " + str(pdata4) + "\r\n")
+
+                        if (packet_type == 'M'):
+                            if (dataString == "IGNITER ARMED"):
+                                window.Element('ARM').Update(button_color=('White', 'Green'))
+                                window.Element('FIRE').Update(button_color=('White', 'Red'))
+                                window.Element('PURGE').Update(button_color=('White', 'Red'))
+                                window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
+                            if (dataString == "IGNITER FIRING"):
+                                window.Element('ARM').Update(button_color=('White', 'Red'))
+                                window.Element('FIRE').Update(button_color=('White', 'Green'))
+                                window.Element('PURGE').Update(button_color=('White', 'Red'))
+                                window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
+                            if (dataString == "IGNITER PURGING"):
+                                window.Element('ARM').Update(button_color=('White', 'Red'))
+                                window.Element('FIRE').Update(button_color=('White', 'Red'))
+                                window.Element('PURGE').Update(button_color=('White', 'Green'))
+                                window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
+                            if (dataString == "IGNITER CLOSED"):
+                                window.Element('ARM').Update(button_color=('White', 'Red'))
+                                window.Element('FIRE').Update(button_color=('White', 'Red'))
+                                window.Element('PURGE').Update(button_color=('White', 'Red'))
+                                window.Element('CLOSE ALL').Update(button_color=('White', 'Green'))
+                            print(dataString)
+                        if (packet_type == 'V'):
+                            for x in range(6):
+                                isValveOpen[x] = bool(int(dataString[x]))
+                            print(dataString)
                     else:
-                        upperNibble = upperNibble & 0b00001111  # upperNibble now equals 0x00 - 0x09
-                    upperNibble = upperNibble << 4  # upperNibble now represents upper byte of dataBytes
-                    # LOWER NIBBLE
-                    if (lowerNibble > 0x40):  # if letter
-                        lowerNibble = lowerNibble & 0b00001111  # strip off upper nibble
-                        lowerNibble = lowerNibble + 0b00001001  # lowerNibble now equals 0x0A - 0x0F
-                    lowerNibble = lowerNibble & 0b00001111  # lowerNibble now equals 0x00 - 0x09 if it was a number
-
-                    packet_dataSize = upperNibble | lowerNibble  # combining upper and lower bytes to make final char(UUUU LLLL)
-                    dataString = buffer[5:-1]  # sets data string of packet to data pulled from _inputString
-                    buffer = ""  # Clear buffer
-
-                    # ***  UPDATE PRESSURE READINGS ***
-                    # ***  CAN WE CHECK THESE CALCULATIONS???
-                    # ***  5V ON PIN = 28 PSI???
-                    if (packet_type == 'D'):
-                        pdata1 = dataString[dataString.find('A') + 1:dataString.find('B')]
-                        pdata1 = int(pdata1) * (5.0 / 1023.0) * (14000.0 / 2500.0)
-                        window.Element('P1').Update(round(pdata1, 2))
-                        f.write(str(datetime.now().time()) + ": Pressure 1: " + str(pdata1) + "\r\n")
-                        pdata2 = dataString[dataString.find('B') + 1:dataString.find('C')]
-                        pdata2 = int(pdata2) * (5.0 / 1023.0) * (14000.0 / 2500.0)
-                        window.Element('P2').Update(round(pdata2, 2))
-                        f.write(str(datetime.now().time()) + ": Pressure 2: " + str(pdata2) + "\r\n")
-                        pdata3 = dataString[dataString.find('C') + 1:dataString.find('D')]
-                        pdata3 = int(pdata3) * (5.0 / 1023.0) * (14000.0 / 2500.0)
-                        window.Element('P3').Update(round(pdata3, 2))
-                        f.write(str(datetime.now().time()) + ": Pressure 3: " + str(pdata3) + "\r\n")
-                        pdata4 = dataString[dataString.find('D') + 1:]
-                        pdata4 = int(pdata4) * (5.0 / 1023.0) * (14000.0 / 2500.0)
-                        window.Element('P4').Update(round(pdata4, 2))
-                        f.write(str(datetime.now().time()) + ": Pressure 4: " + str(pdata4) + "\r\n")
-
-                    if (packet_type == 'M'):
-                        if (dataString == "IGNITER ARMED"):
-                            window.Element('ARM').Update(button_color=('White', 'Green'))
-                            window.Element('FIRE').Update(button_color=('White', 'Red'))
-                            window.Element('PURGE').Update(button_color=('White', 'Red'))
-                            window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
-                        if (dataString == "IGNITER FIRING"):
-                            window.Element('ARM').Update(button_color=('White', 'Red'))
-                            window.Element('FIRE').Update(button_color=('White', 'Green'))
-                            window.Element('PURGE').Update(button_color=('White', 'Red'))
-                            window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
-                        if (dataString == "IGNITER PURGING"):
-                            window.Element('ARM').Update(button_color=('White', 'Red'))
-                            window.Element('FIRE').Update(button_color=('White', 'Red'))
-                            window.Element('PURGE').Update(button_color=('White', 'Green'))
-                            window.Element('CLOSE ALL').Update(button_color=('White', 'Red'))
-                        if (dataString == "IGNITER CLOSED"):
-                            window.Element('ARM').Update(button_color=('White', 'Red'))
-                            window.Element('FIRE').Update(button_color=('White', 'Red'))
-                            window.Element('PURGE').Update(button_color=('White', 'Red'))
-                            window.Element('CLOSE ALL').Update(button_color=('White', 'Green'))
-                        print(dataString)
-                    if (packet_type == 'V'):
-                        for x in range(6):
-                            isValveOpen[x] = bool(int(dataString[x]))
-                else:
-                    buffer = ""  # If only 1 end of packet is found, data somehow corrupted, clear buffer
+                        buffer = ""  # If only 1 end of packet is found, data somehow corrupted, clear buffer
 window.close()
 
